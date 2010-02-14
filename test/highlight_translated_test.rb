e0b6aa7895dcd74d64052948e053269663805b6f
Ravinder Singh <ravinder@ravinder-laptop.(none)>
2010-02-14 13:44:28 +0530

    Adding test case

--- test/highlight_translated_test.rb
+++ test/highlight_translated_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
+require File.dirname(__FILE__) + '/../lib/highlight_translated'
 
 class HighlightTranslatedTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  test "it should return the translated text wrapped inside span tag" do
+    I18n.stubs(:translate_without_hightlight).returns("abc")
+    translated_text = I18n.translate('abc')
+    assert_equal "<span style='color:#00FF00;'>abc</span><!-- Key: abc -->", translated_text
   end
+
+  test "it should return 'translation missing' if the translation is not defined" do
+    translated_text = I18n.translate('abc')
+    assert_equal "<span style='color:#00FF00;'>translation missing: en, abc</span><!-- Key: abc -->", translated_text
+  end
+
 end
